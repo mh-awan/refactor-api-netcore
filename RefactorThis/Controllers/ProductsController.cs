5e6ee8123c6efcda532abb89a741a35fc82380fb
mh-awan <muhammad.awan@protonmail.com>
2020-11-06 02:52:12 +1100

    Clean up models and add controller for options

--- RefactorThis/Controllers/ProductsController.cs
+++ RefactorThis/Controllers/ProductsController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RefactorThis.Models;
@@ -9,6 +7,7 @@ using RefactorThis.Services;
 namespace RefactorThis.Controllers
 {
     [Route("api/[controller]")]
+    [Produces("application/json")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
@@ -24,87 +23,6 @@ namespace RefactorThis.Controllers
         {
             var products = await _productService.GetProductsAsync();
             return products;
-        }
-
-        [HttpGet("{id}")]
-        public Product Get(Guid id)
-        {
-            var product = new Product(id);
-            if (product.IsNew)
-                throw new Exception();
-
-            return product;
-        }
-
-        [HttpPost]
-        public void Post(Product product)
-        {
-            product.Save();
-        }
-
-        [HttpPut("{id}")]
-        public void Update(Guid id, Product product)
-        {
-            var orig = new Product(id)
-            {
-                Name = product.Name,
-                Description = product.Description,
-                Price = product.Price,
-                DeliveryPrice = product.DeliveryPrice
-            };
-
-            if (!orig.IsNew)
-                orig.Save();
-        }
-
-        [HttpDelete("{id}")]
-        public void Delete(Guid id)
-        {
-            var product = new Product(id);
-            product.Delete();
-        }
-
-        [HttpGet("{productId}/options")]
-        public ProductOptions GetOptions(Guid productId)
-        {
-            return new ProductOptions(productId);
-        }
-
-        [HttpGet("{productId}/options/{id}")]
-        public ProductOption GetOption(Guid productId, Guid id)
-        {
-            var option = new ProductOption(id);
-            if (option.IsNew)
-                throw new Exception();
-
-            return option;
-        }
-
-        [HttpPost("{productId}/options")]
-        public void CreateOption(Guid productId, ProductOption option)
-        {
-            option.ProductId = productId;
-            option.Save();
-        }
-
-        [HttpPut("{productId}/options/{id}")]
-        public void UpdateOption(Guid id, ProductOption option)
-        {
-            var orig = new ProductOption(id)
-            {
-                Name = option.Name,
-                Description = option.Description
-            };
-
-            if (!orig.IsNew)
-                orig.Save();
-        }
-
-        [HttpDelete("{productId}/options/{id}")]
-        public void DeleteOption(Guid id)
-        {
-            var opt = new ProductOption(id);
-            opt.Delete();
         }
     }
 }
--- RefactorThis/Models/Product.cs
+++ RefactorThis/Models/Product.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace RefactorThis.Models
@@ -17,57 +18,6 @@ namespace RefactorThis.Models
 
         [JsonIgnore] public bool IsNew { get; }
 
-        public Product()
-        {
-            Id = Guid.NewGuid();
-            IsNew = true;
-        }
-
-        public Product(Guid id)
-        {
-            IsNew = true;
-            var conn = Helpers.NewConnection();
-            conn.Open();
-            var cmd = conn.CreateCommand();
-            cmd.CommandText = $"select * from Products where id = '{id}' collate nocase";
-
-            var rdr = cmd.ExecuteReader();
-            if (!rdr.Read())
-                return;
-
-            IsNew = false;
-            Id = Guid.Parse(rdr["Id"].ToString());
-            Name = rdr["Name"].ToString();
-            Description = (DBNull.Value == rdr["Description"]) ? null : rdr["Description"].ToString();
-            Price = decimal.Parse(rdr["Price"].ToString());
-            DeliveryPrice = decimal.Parse(rdr["DeliveryPrice"].ToString());
-        }
-
-        public void Save()
-        {
-            var conn = Helpers.NewConnection();
-            conn.Open();
-            var cmd = conn.CreateCommand();
-
-            cmd.CommandText = IsNew
-                ? $"insert into Products (id, name, description, price, deliveryprice) values ('{Id}', '{Name}', '{Description}', {Price}, {DeliveryPrice})"
-                : $"update Products set name = '{Name}', description = '{Description}', price = {Price}, deliveryprice = {DeliveryPrice} where id = '{Id}' collate nocase";
-
-            conn.Open();
-            cmd.ExecuteNonQuery();
-        }
-
-        public void Delete()
-        {
-            foreach (var option in new ProductOptions(Id).Items)
-                option.Delete();
-
-            var conn = Helpers.NewConnection();
-            conn.Open();
-            var cmd = conn.CreateCommand();
-
-            cmd.CommandText = $"delete from Products where id = '{Id}' collate nocase";
-            cmd.ExecuteNonQuery();
-        }
+        public IList<ProductOption> ProductOptions { get; set; } = new List<ProductOption>();
     }
 }
